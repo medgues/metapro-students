2864b2233060ad388059133bd13392ab6d34d8b7
medgues <mohamedlamine.guesmia@univ-tebessa.dz>
2024-05-23 17:31:10 +0100

    added descrption to pakcage.json

--- server/index.js
+++ server/index.js
@@ -26,9 +26,6 @@ app.use(express.json());
 app.use("/api/students", studentRouter);
 
 app.use(express.static(path.join(__dirname, "/public")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
